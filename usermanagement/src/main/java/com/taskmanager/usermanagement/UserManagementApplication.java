e2f15ccd8407b4de7ed1774977338178bbfcc544
Atik Sharma <atiksharma1503@gmail.com>
2025-03-20 20:12:55 +0530

    Renamed Artifact and Application name

--- usermanagement/src/main/java/com/taskmanager/usermanagement/UserManagementApplication.java
+++ usermanagement/src/main/java/com/taskmanager/usermanagement/UserManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserSvcApplication {
+public class UserManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserSvcApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(UserManagementApplication.class, args);
+  }
 
 }
