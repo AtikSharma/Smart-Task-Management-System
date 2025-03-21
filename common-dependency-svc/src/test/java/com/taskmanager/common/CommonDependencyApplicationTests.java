e6bd7a3bd9d00668914e6cf69d682f470eca87d4
Atik Sharma <atiksharma1503@gmail.com>
2025-03-21 10:58:19 +0530

    Added common, gateway, eureka, task services

---
+++ common-dependency-svc/src/test/java/com/taskmanager/common/CommonDependencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmanager.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonDependencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
