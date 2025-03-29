82e9e17db40a1b12e77889d76b02999900595ee0
Atik Sharma <atiksharma1503@gmail.com>
2025-03-29 11:04:06 +0530

    Configured Common Dependency

--- common-dependency-svc/src/main/java/com/taskmanager/common/CommonDependencyApplication.java
+++ common-dependency-svc/src/main/java/com/taskmanager/common/CommonDependencyApplication.java
@@ -1,13 +1,8 @@
 package com.taskmanager.common;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class CommonDependencyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CommonDependencyApplication.class, args);
 	}
 
 }
