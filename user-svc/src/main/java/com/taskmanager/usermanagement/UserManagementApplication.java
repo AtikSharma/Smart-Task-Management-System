54daf953c535b7342ae8462e8f5375c2768e27e9
Atik Sharma <atiksharma1503@gmail.com>
2025-03-29 11:10:37 +0530

    Configured DataBase and Eureka in Task and User service

--- user-svc/src/main/java/com/taskmanager/usermanagement/UserManagementApplication.java
+++ user-svc/src/main/java/com/taskmanager/usermanagement/UserManagementApplication.java
@@ -2,12 +2,14 @@ package com.taskmanager.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class UserManagementApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(UserManagementApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
 
 }
